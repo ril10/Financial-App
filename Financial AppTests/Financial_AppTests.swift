dea1b245f8913b0c06dc4fa486dcf1979d80d162
Sergey Luk <jlamep7@gmail.com>
2021-10-18 18:17:36 +0300

    update

--- Financial AppTests/Financial_AppTests.swift
+++ Financial AppTests/Financial_AppTests.swift
@@ -10,12 +10,15 @@ import XCTest
 
 class Financial_AppTests: XCTestCase {
 
+    var sut : Date!
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        sut = Date()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+        try super.tearDownWithError()
     }
 
     func testExample() throws {
@@ -28,6 +31,16 @@ class Financial_AppTests: XCTestCase {
         self.measure {
             // Put the code you want to measure the time of here.
         }
+    }
+    
+    func testDateFormatter() throws {
+        //given
+        let receivedDateTimeInterval = Date().timeIntervalSince1970
+
+        //when
+        
+        //then
+        
     }
 
 }
